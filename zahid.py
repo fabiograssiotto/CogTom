5ff063cce531ecab2a57a1cd920f605c00ce61ff
Fabio Grassiotto <fabiogr@gmail.com>
2019-08-12 17:33:06 -0300

    Changed implementation to use pandas and text file

--- zahid.py
+++ zahid.py
@@ -2,14 +2,8 @@
 # according to Simon Baron Cohen.
 # The intent of this software program is to evaluate false belief tasks.
 
+import pandas as pd
+
 # Basic input for scene
-num_actors = input("Enter the number of actors\n")
-actors = list()
-objects = list()
-for i in range(int(num_actors)):
-    actor = input("Enter Actor #" + str(i+1) + ": ")
-    actors.append(actor)
-num_objects = input("Enter the number of objects\n")
-for i in range(int(num_objects)):
-    object = input("Enter Object #" + str(i+1) + ": ")
-    objects.append(object)
+# Reads from format: Actor-sees-Object / Actor-sees-Actor
+data = pd.read_csv('environment.txt', sep="\t", comment='#')
