f474d8efaa4cfad998dac0e0d621b00ed60fccc6
Fabio Grassiotto <fabiogr@gmail.com>
2019-08-13 22:19:27 -0300

    Corrected parsing for visual field

--- zahid.py
+++ zahid.py
@@ -1,5 +1,4 @@
-# Zahid - an implementation of the Theory of Mind model
-# according to Simon Baron Cohen.
+# Zahid - an implementation of the Theory of Mind model according to Simon Baron Cohen.
 # The intent of this software program is to evaluate false belief tasks.
 import time
 import pandas as pd
@@ -7,17 +6,19 @@ import pandas as pd
 # Constants
 sleep_time = 5 # 5 seconds sleep time between Mind evaluations.
 
-# Basic input for scene
-# Start by accessing the camera input and identifying entities in the scene.
-entities = pd.read_csv('visual.txt', sep="\t", comment='#')
-
-# Reads from format: Actor-sees-Object / Actor-sees-Actor
-data = pd.read_csv('environment.txt', sep="\t", comment='#')
+# Basic input for scene. Start by accessing the camera input and identifying entities
+# in the scene.
+ent_df = pd.read_csv('visual.txt',
+                     delim_whitespace=True,
+                     comment='#')
 
 # Start Mind Loop, evaluating at each time t.
 t = 1
 while (True):
     print("Evaluating Mind Step ", t)
+
+    # First step is selecting entities for the current time step.
+    entities = ent_df.loc[ent_df['t'] == t]
+    print(entities)
     time.sleep(sleep_time)
     t = t + 1
-
