802a926bb8aba09f4de1b434b50630a43db8c34e
Fabio Grassiotto <fabiogr@gmail.com>
2019-08-20 22:39:09 -0300

    Implement EDD_Eye_Direction

--- edd.py
+++ edd.py
@@ -11,6 +11,45 @@ class Edd:
         entities_arr = np.array(entities['Entity'].tolist())
         is_agent_arr = np.array(entities['Is_Moving'].tolist())
 
-        edd_entities = np.column_stack((entities_arr, is_agent_arr))
+        self.edd_entities = np.column_stack((entities_arr, is_agent_arr))
         # Assume that all agents have eyes.
-        edd_eyes = np.array(agents)
+        self.edd_eyes = np.array(agents)
+
+    def process(self, eye_dir):
+        # Dimensions for the EDD Eye Direction Array are (agent) lines
+        # and (entities) columns.
+        lines = self.edd_eyes.shape[0]
+        cols = self.edd_entities.shape[0] 
+
+        self.edd_eye_dir = np.zeros((lines, cols))
+        # Fill in Eye Direction Array based on the visual system.
+        eye_dir_arr = eye_dir.to_numpy()
+        for entry in range(eye_dir_arr.shape[0]):
+            # For each line in the visual system, identify the object 
+            # and fill in the EDD eye direction array.
+            # By convention the array is configured as:
+            #         Agent1 Obj1 Obj2 Obj3
+            # Agent1     0    0/1  0/1  0/1
+            # Agent2     0    0/1  0/1  0/1
+            # where 0 is no gaze and 1 is a gaze.
+            agent = eye_dir_arr[entry, 0]
+            obj = eye_dir_arr[entry, 1]
+            # identify row for insertion
+            l = np.where(self.edd_eyes == agent)[0]
+            c =  np.where(self.edd_entities == obj)[0]
+            # Set as '1' to identify a object that is in the visual field of the agent.
+            self.edd_eye_dir[l, c] = 1
+    
+    def print(self):
+        # Basic debugging
+        print("EDD_Eye_Direction: ", self.edd_eye_dir)
+
+
+
+
+
+        
+
+        
+
+
--- zahid.py
+++ zahid.py
@@ -7,7 +7,6 @@ import pandas as pd
 from id import Id
 from edd import Edd
 
-
 # Constants
 sleep_time = 5 # 5 seconds sleep time between Mind evaluations.
 
@@ -17,6 +16,11 @@ ent_df = pd.read_csv('visual.txt',
                      delim_whitespace=True,
                      comment='#')
 
+# Eye Direction System, identifies which entities are in the visual field of an agent.
+eye_dir_df = pd.read_csv('eye_direction.txt',
+                         delim_whitespace=True,
+                         comment='#')
+
 # Start Mind Loop, evaluating at each time t.
 t = 1
 while (True):
@@ -25,14 +29,21 @@ while (True):
     # First step is selecting entities for the current time step,
     # and getting rid of the time information.
     entities = ent_df.loc[ent_df['t'] == t]
-    entities.drop(columns='t')
+    entities = entities.drop(columns='t')
+    # Do the same operation to the eye direction information
+    # for the current time step.
+    eye_dir = eye_dir_df.loc[eye_dir_df['t'] == t]
+    eye_dir = eye_dir.drop(columns='t')
     
     # Create ID module
     id = Id(entities)
-    id.print()
+    id.print() # Prints Agents
 
     # Create EDD module
     edd = Edd(entities, id.agents())
+    # Request Eye Direction Processing
+    edd.process(eye_dir)
+    edd.print() # Prints Eye Direction Array
 
     time.sleep(sleep_time)
     t = t + 1
