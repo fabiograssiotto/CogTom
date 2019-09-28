2f687c81866a6253bb824c4ac4cd8b16a4de45ee
Fabio Grassiotto <fabiogr@gmail.com>
2019-09-28 09:39:04 -0300

    Added Belief based on affordances

--- tom.py
+++ tom.py
@@ -6,7 +6,7 @@ import numpy as np
 class ToM:
 
     # The set of mental states ToM represents.
-    MENTAL_STATES = ["SEES", "BELIEVES"]
+    MENTAL_STATES = ["Believes"]
 
     def __init__(self, affordances, id, edd, sam):
         self.afford = affordances.values.tolist() # List of affordances for the objects in the environment
@@ -20,7 +20,9 @@ class ToM:
         # Create representations of the mental states 
         # of the form Agent-Mental State-Object
 
-        #'SEES' mental state.
+        #'Believes' mental state
+        # The mental state modeled here produces descriptions in the form
+        # AGENT BELIEVES OBJECT-STATE
         for agent in range(self.edd.edd_eyes.shape[0]):
             tom_belief = []
             # For each agent line in edd_eyes
@@ -28,11 +30,16 @@ class ToM:
             tom_belief.append(self.edd.edd_eyes[agent])
             tom_belief.append(self.MENTAL_STATES[0])
             for ent in range(self.edd.edd_agent_store.shape[1]):
-                tom_belief.append(self.edd.edd_agent_store[agent][ent])
+                obj = self.edd.edd_agent_store[agent][ent]
+                tom_belief.append(obj)
+                for sublist in self.afford:
+                    if sublist[0] == obj:
+                        # Object has an affordance.
+                        tom_belief.append(sublist[1])
+                        break
                 self.tom_beliefs.append(tom_belief.copy())
-                del tom_belief[-1]
-
-        #'BELIEVES' mental state
+                del tom_belief[2:] # Only keeps Agent and Belief
+    
         
 
     def print(self):
